c8b80a8b46d5219494056086c73c89ef5d9326b4
Hermann Krumrey <hermann@krumreyh.com>
2016-09-29 05:19:46 +0200

    Enhanced the CLI arguments

--- comunio/main.py
+++ comunio/main.py
@@ -27,6 +27,7 @@ import argparse
 from comunio.ui.StatisticsViewer import start as start_gui
 from comunio.scraper.ComunioSession import ComunioSession
 from comunio.database.DatabaseManager import DatabaseManager
+from comunio.calc.StatisticsCalculator import StatisticsCalculator
 
 
 def main() -> None:
@@ -39,13 +40,25 @@ def main() -> None:
     parser.add_argument("username",  help="The username with which to log in to comunio.de")
     parser.add_argument("password", help="The password with which to log in to comunio.de")
     parser.add_argument("-g", "--gui", action="store_true", help="Starts the program in GUI mode")
+    parser.add_argument("-u", "--update", action="store_true", help="Only updates the database, then quits")
+    parser.add_argument("-s", "--summary", action="store_true", help="Lists the current state of the comunio account")
     args = parser.parse_args()
 
     comunio = ComunioSession(args.username, args.password)
     database = DatabaseManager(comunio)
+    calculator = StatisticsCalculator(comunio, database)
 
     if args.gui:
         start_gui(comunio, database)
+    elif args.list:
+        print("Cash:       {:,}".format(database.get_last_cash_amount()))
+        print("Team value: {:,}".format(database.get_last_team_value_amount()))
+        print("Balance:    {:,}".format(calculator.calculate_total_assets_delta()))
+        print("\n\nPlayers:\n")
+        for player in database.get_players_on_day(0):
+            print(player)
+    elif args.update:
+        database.update_database()
 
 if __name__ == '__main__':
     main()
